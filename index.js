fb62c49e2ccb27e68bd273aa045c8b63f18c4470
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-07-10 20:08:04 -0300

    feat: Adding the card to the form

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
 
 // Para o express usar EJS como View engine.
 app.set("view engine", "ejs");
 
+app.use(express.static("public"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.render("index");
-})
+});
+
+app.get("/perguntar", (req, res) => {
+  res.render("perguntar");
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`);
-})
+});
