7f6676b8d7a78fe4afdc791dcf21d15ad19e34e4
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-07-12 14:45:12 -0300

    feat: Capturing form data

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
 const PORT = process.env.PORT || 3000;
 
 // Para o express usar EJS como View engine.
@@ -8,12 +10,21 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.json());
 
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.render("index");
 });
 
 app.get("/perguntar", (req, res) => {
   res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req, res) => {
+  const titulo = req.body.titulo;
+  const descricao = req.body.descricao;
+  res.send(`formulario recebido! Titulo: ${titulo}, descrição ${descricao}`);
 });
 
 app.listen(PORT, () => {
